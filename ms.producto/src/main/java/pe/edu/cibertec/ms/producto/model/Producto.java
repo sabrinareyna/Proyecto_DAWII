1c30ac7cdd35bf1e1d853feb48bee9a89512c92c
ashlyy09 <ashlyatt9@gmail.com>
2025-10-09 00:57:29 -0500

    Update Producto.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/Producto.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/Producto.java
@@ -1,5 +1,45 @@
 package pe.edu.cibertec.ms.producto.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name ="PRODUCTO")
 public class Producto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODPRODUCTO", nullable = false)
+    private Long idpro;
+
+    // 🔹 Relación con CATEGORIA
+    @ManyToOne
+    @JoinColumn(name = "CODCATEGORIA", nullable = false)
+    private Categoria idcat;
+
+    @Column(name = "IMGPRODUCTO", nullable = false, length = 50)
+    private String imgprod;
+
+    @Column(name = "NOMPRODUCTO", nullable = false, length = 70)
+    private String nomprod;
+
+    @Column(name = "DESCRIPCION", nullable = false, length = 300)
+    private String desprod;
+
+    @Column(name = "PREUNI", nullable = false, precision = 10, scale = 2)
+    private BigDecimal preuni;
+
+    // Relación con MARCA
+    @ManyToOne
+    @JoinColumn(name = "CODMARCA", nullable = false)
+    private Marca id;
+
+    @Column(name = "STOCK", nullable = false)
+    private int stock;
+
+    @Column(name = "ESTPRODUCTO", nullable = false)
+    private boolean estProducto;
 }
