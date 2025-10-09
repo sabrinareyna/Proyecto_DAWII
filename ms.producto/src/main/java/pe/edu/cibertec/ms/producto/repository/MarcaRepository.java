f928bbfd50307f53f17b8d16a424d080235c4f99
ashlyy09 <ashlyatt9@gmail.com>
2025-10-09 03:11:25 -0500

    Update MarcaRepository.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/MarcaRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/MarcaRepository.java
@@ -3,12 +3,11 @@ package pe.edu.cibertec.ms.producto.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import pe.edu.cibertec.ms.pedido.model.Marca;
+import pe.edu.cibertec.ms.producto.model.Marca;
 
 import java.util.List;
 
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
-
     // Llama al procedimiento que lista las marcas
     @Query(value = "CALL USP_GET_MARCA()", nativeQuery = true)
     List<Marca> getMarcas();
@@ -18,6 +17,6 @@ public interface MarcaRepository extends JpaRepository<Marca, Long> {
     List<Object[]> selectMarcas();
 
     // Llama al procedimiento que obtiene una marca por ID
-    @Query(value = "CALL USP_GET_ID_MARCA(:id)", nativeQuery = true)
-    Marca getMarcaById(@Param("id") int id);
+    @Query(value = "CALL USP_GET_ID_MARCA(:codMarca)", nativeQuery = true)
+    Marca getMarcaById(@Param("codMarca") int codMarca);
 }
