d8b8c653b9b72ffbef90d8b9c0dab627f435a94c
Sabrina <sabrinareynaaa@gmail.com>
2025-10-08 23:35:41 -0500

    marca

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/marca.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/marca.java
@@ -1,5 +1,23 @@
 package pe.edu.cibertec.ms.producto.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "MARCA")
+@Data
 public class marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODMARCA", nullable = false)
+    private Long id;
+
+    @Column(name = "NOMBREMARCA", nullable = false)
+    private String nombre;
+
+    @Column(name = "ESTMARCA", nullable = false)
+    private boolean estado;
+
 
 }
