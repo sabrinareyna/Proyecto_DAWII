07d50b5195423971af56dbd781119085f2c43756
Sabrina <sabrinareynaaa@gmail.com>
2025-10-09 00:48:57 -0500

    repository

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/categoriaRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/categoriaRepository.java
@@ -1,4 +1,4 @@
 package pe.edu.cibertec.ms.producto.repository;
 
-public interface MarcaRepository {
+public interface categoriaRepository {
 }
--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/marcaRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/marcaRepository.java
@@ -1,4 +1,4 @@
 package pe.edu.cibertec.ms.producto.repository;
 
-public interface ProductoRepository {
+public interface marcaRepository {
 }
