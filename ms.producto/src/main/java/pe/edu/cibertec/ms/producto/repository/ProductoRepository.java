83a4b9f7432dec2fd198960b825057d80bc0bce4
ashlyy09 <ashlyatt9@gmail.com>
2025-10-09 01:13:00 -0500

    Update ProductoRepository.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/ProductoRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/ProductoRepository.java
@@ -1,4 +1,6 @@
 package pe.edu.cibertec.ms.producto.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.cibertec.ms.pedido.model.Producto;
 
-public interface ProductoRepository {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
