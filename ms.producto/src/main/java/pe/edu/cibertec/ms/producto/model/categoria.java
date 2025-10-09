f5ffce5baa531919c8b515f986f10bf4207be954
ashlyy09 <ashlyatt9@gmail.com>
2025-10-09 00:15:51 -0500

    Update categoria.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/categoria.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/categoria.java
@@ -1,5 +1,20 @@
 package pe.edu.cibertec.ms.producto.model;
 
-public class categoria {
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Data
+@Table(name ="CATEGORIA")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CODCATEGORIA;
+
+    private String IMGCATEGORIA;
+
+    private String NOMCATEGORIA;
+
+    private String ESTCATEGORIA;
 }
