eb85aea830528501359276e71529cb478be63814
ashlyy09 <ashlyatt9@gmail.com>
2025-10-09 03:18:56 -0500

    Update CategoriaRepository.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/CategoriaRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/CategoriaRepository.java
@@ -1,7 +1,12 @@
 package pe.edu.cibertec.ms.producto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.cibertec.ms.pedido.model.Categoria;
+import org.springframework.data.jpa.repository.Query;
+import pe.edu.cibertec.ms.producto.model.Categoria;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    @Query(value = "CALL USP_SELECT_CATEGORIAS()", nativeQuery = true)
+    List<Object[]> selectCategorias();
 }
