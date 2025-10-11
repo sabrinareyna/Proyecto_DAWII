3a4b1290b89d2e89907819bded6d2e219de87629
ashlyy09 <ashlyatt9@gmail.com>
2025-10-11 16:35:12 -0500

    Update Marca.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/Marca.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/model/Marca.java
@@ -4,17 +4,18 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "MARCA")
 @Data
+@Table(name = "MARCA")
 public class Marca {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CODMARCA", nullable = false)
     private Long codMarca;
 
     @Column(name = "NOMBREMARCA", nullable = false)
-    private String nombre;
+    private String nombreMarca;
 
     @Column(name = "ESTMARCA", nullable = false)
-    private boolean estado;
+    private boolean estMarca;
 }
