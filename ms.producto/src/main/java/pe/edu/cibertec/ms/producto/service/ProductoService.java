aeef0be66e5724586a4802cba1ebeb6acf84a16d
ashlyy09 <ashlyatt9@gmail.com>
2025-10-11 16:41:10 -0500

    Update ProductoService.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/service/ProductoService.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/service/ProductoService.java
@@ -1,4 +1,68 @@
 package pe.edu.cibertec.ms.producto.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.ms.producto.model.Producto;
+import pe.edu.cibertec.ms.producto.repository.ProductoRepository;
+
+import java.util.List;
+
+@Service
 public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> listarPorCategoria(int codCategoria) {
+        return productoRepository.listarPorCategoria(codCategoria);
+    }
+
+    public List<Producto> listarTop5MasBaratos() {
+        return productoRepository.listarTop5MasBaratos();
+    }
+
+    public Producto obtenerPorId(int codProducto) {
+        return productoRepository.obtenerPorId(codProducto);
+    }
+
+    public String registrarProducto(Producto producto) {
+        productoRepository.registrarOActualizarProducto(
+                0,
+                producto.getCategoria().getCodCategoria().intValue(),
+                producto.getImgProducto(),
+                producto.getNomProducto(),
+                producto.getDescripcion(),
+                producto.getPreUni().doubleValue(),
+                producto.getMarca().getCodMarca().intValue(),
+                producto.getStock(),
+                producto.isEstProducto()
+        );
+        return "Producto registrado correctamente.";
+    }
+
+    public String actualizarProducto(Producto producto) {
+        productoRepository.registrarOActualizarProducto(
+                producto.getCodProducto().intValue(),
+                producto.getCategoria().getCodCategoria().intValue(),
+                producto.getImgProducto(),
+                producto.getNomProducto(),
+                producto.getDescripcion(),
+                producto.getPreUni().doubleValue(),
+                producto.getMarca().getCodMarca().intValue(),
+                producto.getStock(),
+                producto.isEstProducto()
+        );
+        return "Producto actualizado correctamente.";
+    }
+
+    public String cambiarEstadoProducto(int codProducto, boolean estadoActual) {
+        if (estadoActual) {
+            productoRepository.eliminarProducto(codProducto);
+            return "Producto deshabilitado correctamente.";
+        } else {
+            productoRepository.habilitarProducto(codProducto);
+            return "Producto habilitado correctamente.";
+        }
+    }
 }
+
