8c7a7d5f6b02085c24143c82b750bf4ab1982762
ashlyy09 <ashlyatt9@gmail.com>
2025-10-14 22:30:28 -0500

    Update ProductoRepository.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/ProductoRepository.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/repository/ProductoRepository.java
@@ -1,42 +1,54 @@
 package pe.edu.cibertec.ms.producto.repository;
-import org.springframework.data.jpa.repository.query.Procedure;
+
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.ms.producto.dto.ProductoDTO;
 import pe.edu.cibertec.ms.producto.model.Producto;
 
 import java.util.List;
 
 @Repository
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
+public interface ProductoRepository extends CrudRepository<Producto, Integer> {
 
-    @Procedure("USP_GET_PRODUCTO")
-    List<Producto> listarProductos();
 
-    @Procedure("USP_GET_PRODUCTO_POR_CATEGORIA")
-    List<Producto> listarPorCategoria(int CodCategoria);
+    @Query(value = "CALL USP_GET_PRODUCTO()", nativeQuery = true)
+    List<ProductoDTO> getProductos();
 
-    @Procedure("USP_GET_TOP5_PRODUCTOS_MAS_BARATOS")
-    List<Producto> listarTop5MasBaratos();
+    @Query(value = "CALL USP_GET_TOP5_PRODUCTOS_MAS_BARATOS()", nativeQuery = true)
+    List<ProductoDTO> getTop5ProductosMasBaratos();
 
-    @Procedure("USP_GET_ID_PRODUCTO")
-    Producto obtenerPorId(int CodProducto);
+    @Query(value = "CALL USP_GET_PRODUCTOS_POR_PEDIDO(:CODPEDIDO)", nativeQuery = true)
+    List<Object[]> getProductosByPedido(@Param("CODPEDIDO") Integer codPedido);
 
-    @Procedure("USP_MERGE_PRODUCTO")
-    void registrarOActualizarProducto(
-            int CodProducto,
-            int CodCategoria,
-            String ImgProducto,
-            String NomProducto,
-            String Descripcion,
-            double PreUni,
-            int CodMarca,
-            int Stock,
-            boolean EstProducto
+    @Query(value = "CALL USP_GET_PRODUCTO_POR_CATEGORIA(:CodCategoria)", nativeQuery = true)
+    List<ProductoDTO> getProductosPorCategoria(@Param("CodCategoria") Integer codCategoria);
+
+    @Query(value = "CALL USP_GET_ID_PRODUCTO(:CODPRODUCTO)", nativeQuery = true)
+    ProductoDTO getProducto(@Param("CODPRODUCTO") Integer codProducto);
+
+    @Modifying
+    @Query(value = "CALL USP_MERGE_PRODUCTO(:CODPRODUCTO, :CODCATEGORIA, :IMGPRODUCTO, :NOMPRODUCTO, :DESCRIPCION, :PREUNI, :CODMARCA, :STOCK, :ESTPRODUCTO)", nativeQuery = true)
+    void mergeProducto(
+            @Param("CODPRODUCTO") Integer codProducto,
+            @Param("CODCATEGORIA") Integer codCategoria,
+            @Param("IMGPRODUCTO") String imgProducto,
+            @Param("NOMPRODUCTO") String nomProducto,
+            @Param("DESCRIPCION") String descripcion,
+            @Param("PREUNI") Double preUni,
+            @Param("CODMARCA") Integer codMarca,
+            @Param("STOCK") Integer stock,
+            @Param("ESTPRODUCTO") Boolean estProducto
     );
 
-    @Procedure("USP_DELETE_PRODUCTO")
-    void eliminarProducto(int CodProducto);
+    @Modifying
+    @Query(value = "CALL USP_DELETE_PRODUCTO(:CODPRODUCTO)", nativeQuery = true)
+    void deleteProducto(@Param("CODPRODUCTO") Integer codProducto);
 
-    @Procedure("USP_ENABLE_PRODUCTO")
-    void habilitarProducto(int CodProducto);
+    @Modifying
+    @Query(value = "CALL USP_ENABLE_PRODUCTO(:p_codproducto)", nativeQuery = true)
+    void enableProducto(@Param("p_codproducto") Integer codProducto);
 }
