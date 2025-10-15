77e1bd8b8618f0b72462db3e9dddf9fdd3b15871
ashlyy09 <ashlyatt9@gmail.com>
2025-10-14 22:35:49 -0500

    Update ProductoRequestDTO.java

--- ms.producto/src/main/java/pe/edu/cibertec/ms/producto/dto/ProductoRequestDTO.java
+++ ms.producto/src/main/java/pe/edu/cibertec/ms/producto/dto/ProductoRequestDTO.java
@@ -1,4 +1,17 @@
 package pe.edu.cibertec.ms.producto.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductoRequestDTO {
+    private Integer codCategoria;
+    private Integer codMarca;
+    private String imgProducto;
+    private String nomProducto;
+    private String descripcion;
+    private BigDecimal preUni;
+    private Integer stock;
+    private Boolean estProducto;
 }
