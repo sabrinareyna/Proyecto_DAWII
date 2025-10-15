cf8e85e87e74103823e3061aea8e788097d2d546
Sabrina <sabrinareynaaa@gmail.com>
2025-10-14 20:29:48 -0500

    .

---
+++ ms.auth/src/test/java/edu/pe/cibertec/ms/auth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.ms.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
